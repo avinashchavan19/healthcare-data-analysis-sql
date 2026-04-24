5eaf48aa3398d67503b7a055528af496df8b3f21
Avinash Chavan <avinashchavan1917@gmail.com>
2026-04-24 19:00:42 +0530

    Update 02_revenue_analysis.sql

--- queries/02_revenue_analysis.sql
+++ queries/02_revenue_analysis.sql
@@ -1,1 +1,12 @@
+-- Revenue Analysis
 
+SELECT treatment_type, SUM(cost) AS total_revenue
+FROM treatments
+GROUP BY treatment_type
+ORDER BY total_revenue DESC;
+
+-- Top Patients
+SELECT patient_id, SUM(cost) AS total_spent
+FROM treatments
+GROUP BY patient_id
+ORDER BY total_spent DESC;
